c8997bdd3f4e8b095b87908765c5b590d51138ff
Winston Weinert <git@winny.tech>
2022-07-09 16:35:25 -0500

    actions(toml-test): save artifact

--- scripts/run-toml-test.bash
+++ scripts/run-toml-test.bash
@@ -8,7 +8,8 @@ cmd_usage() {
     printf '%s: Run toml-tests against the racket toml library.
 %s: usage: %s [-n]
 
-  -n : disable color output
+  -n        : disable color output
+  -l <file> : log to file
 '
     case $* in
         --help|-h|help) exit 0 ;;
@@ -17,19 +18,34 @@ cmd_usage() {
 }
 
 cmd_default() {
+    while getopts "nl:" arg; do
+        case $arg in
+            n)
+                export NO_COLOR='YES PLS'
+                ;;
+            l)
+                log_to="$OPTARG"
+                ;;
+            *)
+                cmd_usage "$@"
+                ;;
+        esac
+    done
+    shift $((OPTIND-1))
+
     # My version of toml-test does not support 0.2.0
-    toml-test -toml 0.4.0 -- racket -l toml/compliance/decoder
+    cmd=(
+        toml-test
+        -toml 0.4.0
+        --
+        racket -l toml/compliance/decoder
+    )
+
+    if [[ ${log_to+present} ]]; then
+        "${cmd[@]}" 2>&1 | tee "$log_to"
+    else
+        "${cmd[@]}"
+    fi
 }
 
-while getopts "n" arg; do
-    case $arg in
-        n)
-            export NO_COLOR='YES PLS'
-            ;;
-        *)
-            cmd_usage
-            ;;
-    esac
-done
-
-cmd_default
+cmd_default "$@"
