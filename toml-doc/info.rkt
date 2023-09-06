75ccf3afb2d757fb62cbc40da3d0a6735c9ab98b
Benjamin Yeung <bzyeung@gmail.com>
2023-09-06 19:37:13 -0400

    Fix dependency issues with `toml-doc`

--- toml-doc/info.rkt
+++ toml-doc/info.rkt
@@ -1,7 +1,9 @@
 #lang info
 
+(define deps '("base"))
 (define build-deps '("base"
                      "scribble-lib"
+		     "racket-doc"
                      "toml-lib"))
 
 (define scribblings '(("scribblings/toml.scrbl" () (parsing-library))))
